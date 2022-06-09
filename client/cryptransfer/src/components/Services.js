c49ffb83c6f0789a54503363d1911efdc356fb79
PadmanabhKhunt <padmanabhkhunt@gmail.com>
2022-06-09 10:36:06 +0530

    services

--- client/cryptransfer/src/components/Services.js
+++ client/cryptransfer/src/components/Services.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import { BsShieldFillCheck } from 'react-icons/bs'
+import { BiSearchAlt } from 'react-icons/bi';
+import { RiHeartFill } from 'react-icons/ri';
+
+const ServicesCard = ({color, title, icons, subtitle}) => (
+    <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+      <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}>
+      {icons}
+      </div>
+      <div className="ml-5 flex flex-column flex-1">
+        <h1 className="ml-2 text-white text-lg">{title}</h1>
+        <p className="ml-2 text-white text-sm md:w-9/12">{subtitle}</p>
+      </div>
+    </div>
+);
 
 const Services = () => {
   return (
-    <h1>Services</h1>
-  )
+    <div className="flex flex-col md:flec-row flex w-full justify-center items-center gradient-bg-services">
+      <div className="flex mf : flex-row flex-col  items-center justify-between md:p-20 py-12 px-4">
+        <div className="flex-1 flex flex-col justify-start items-start">
+          <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">Services that we
+          <br/>
+          continue to improve 
+          </h1>
+        </div>
+      </div> 
+      <div className="flex-1 flex flex-col justify-start items-center">
+        <ServicesCard
+        color="bg-[#2952E3]"
+        title="Security Guaranteed"
+        icons={<BsShieldFillCheck fontSize={21} className="text-white"/>}  
+        subtitle="Security is guaranted. We always maintain privacy and maintain quality of service."
+        />
+        <ServicesCard
+        color="bg-[#8945F8]"
+        title="Best exchange rates"
+        icons={<BiSearchAlt fontSize={21} className="text-white"/>}
+        subtitle="Security is guaranted. We always maintain privacy and maintain quality of service."
+        />
+        <ServicesCard
+        color="bg-[#F84550]"
+        title="Fastest transactions"
+        icons={<RiHeartFill fontSize={21} className="text-white"/>}
+        subtitle="Security is guaranted. We always maintain privacy and maintain quality of service."
+        />
+      </div>
+    </div> 
+  );
 }
 
 export default Services;
